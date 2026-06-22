b40db8573ffeafd3ca86df45c74a85d60be3d885
JhonAlisHarol <aharolj@gmail.com>
2026-06-22 15:25:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -265,7 +265,7 @@ with st.sidebar:
         
         components.html(
             """
-            <div id="reloj" style="font-size: 24px; font-weight: bold; color: #800080;"></div>
+            <div id="reloj" style="font-size: 24px; font-weight: bold; color: #FFFF00;"></div>
             <script>
                 function actualizarReloj() {
                     const ahora = new Date();
